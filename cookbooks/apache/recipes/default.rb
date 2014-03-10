8e82fd1c94dd4e85a6bc51f92ad867af099f1a94
Cory Stephenson <aevin@me.com>
2014-03-10 15:59:58 -0500

    Setup apache cookbook to install and start apache.

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,3 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# install apache
+package "apache2" do
+  action :install
+end
+
+# start the apache service
+# make sure the service starts on reboot
+service "apache2" do
+  action [:start, :enable]
+end
+
+# write our home page
+cookbook_file "/var/www/index.html" do
+  source "index.html"
+  mode "0644"
+end
